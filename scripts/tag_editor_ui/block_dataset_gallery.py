bfbfc7ca456b4589fe8328b293ce745c6ad4f224
Squibeel <52175357+Squibeel@users.noreply.github.com>
2025-04-13 15:15:06 +0100

    Update block_dataset_gallery.py

--- scripts/tag_editor_ui/block_dataset_gallery.py
+++ scripts/tag_editor_ui/block_dataset_gallery.py
@@ -19,7 +19,6 @@ class DatasetGalleryUI(UIBase):
         with gr.Row(visible=False):
             self.cbg_hidden_dataset_filter = gr.CheckboxGroup(label='Dataset Filter')
             self.nb_hidden_dataset_filter_apply = gr.Number(label='Filter Apply', value=-1)
-            self.btn_hidden_set_index = gr.Button(elem_id="dataset_tag_editor_btn_hidden_set_index")
             self.nb_hidden_image_index = gr.Number(value=None, label='hidden_idx_next')
             self.nb_hidden_image_index_prev = gr.Number(value=None, label='hidden_idx_prev')
         self.gl_dataset_images = gr.Gallery(label='Dataset Images', elem_id="dataset_tag_editor_dataset_gallery", columns=image_columns)
@@ -32,29 +31,6 @@ class DatasetGalleryUI(UIBase):
             outputs=[self.nb_hidden_image_index, self.nb_hidden_image_index_prev]
         )
 
-        def set_index_clicked(next_idx: int, prev_idx: int):
-            prev_idx = int(prev_idx) if prev_idx is not None else -1
-            next_idx = int(next_idx) if next_idx is not None else -1
-            img_paths = dte_instance.get_filtered_imgpaths(filters=get_filters())
-            
-            if prev_idx < 0 or len(img_paths) <= prev_idx:
-                prev_idx = -1
-
-            if 0 <= next_idx and next_idx < len(img_paths):
-                self.selected_path = img_paths[next_idx]
-            else:
-                next_idx = -1
-                self.selected_path = ''
-
-            gallery_state.register_value('Selected Image', self.selected_path)
-            return [next_idx, prev_idx]
-        
-        self.btn_hidden_set_index.click(
-            fn=set_index_clicked,
-            _js="(x, y) => [dataset_tag_editor_gl_dataset_images_selected_index(), x]",
-            inputs=[self.nb_hidden_image_index, self.nb_hidden_image_index_prev],
-            outputs=[self.nb_hidden_image_index, self.nb_hidden_image_index_prev]
-        )
         self.nb_hidden_image_index.change(
             fn=self.func_to_set_value('selected_index', int),
             inputs=self.nb_hidden_image_index
@@ -70,3 +46,14 @@ class DatasetGalleryUI(UIBase):
             inputs=[self.cbg_hidden_dataset_filter, self.nb_hidden_dataset_filter_apply],
             outputs=[self.cbg_hidden_dataset_filter, self.nb_hidden_dataset_filter_apply]
         )
+
+        self.gl_dataset_images.select(
+            fn=self.on_gallery_select,
+            inputs=None,
+            outputs=[self.nb_hidden_image_index]
+        )
+   
+    def on_gallery_select(self, evt: gr.SelectData):
+        selected_index = evt.index if evt else -1
+        self.selected_index = selected_index
+        return selected_index
